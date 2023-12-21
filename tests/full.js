2676887d23bb279c412d8510f2434577c2c2b86f
Bob Varioa <true.email001@gmail.com>
2023-12-21 13:14:28 -0600

    fully implement tokenizer and parser

---
+++ tests/full.js
@@ -0,0 +1,81 @@
+
+// numbers
+var a=3;
+let b = 3;
+const c = 3;
+
+a + a;
+a * a;
+a - a;
+a / a;
+a % a;
+a ** a;
+a ++;
+a --;
+
+b += a;
+b *= a;
+b -= a;
+b /= a;
+b %= a;
+b **= a;
+
+a == a;
+a === a;
+a != a;
+a !== a;
+a > a;
+a < a;
+a >= a;
+a <= a;
+a ?? b;
+
+a ??= b;
+a ||= b;
+a &&= b;
+
+a & a;
+a | a;
+~a;
+a ^ a;
+a << a;
+a >> a;
+a >>> a;
+
+a &= a;
+a |= a;
+a ^= a;
+a <<= a;
+a >>= a;
+a >>>= a;
+
+(a);
+
+// strings
+let s1 = "a";
+let s2 = 'b';
+let s3 = "c";
+
+s1 + s2;
+s1 < s2;
+s3 += s1;
+s3 += a;
+
+
+// bools
+
+let b1 = true;
+let b2 = false;
+
+b1 && b1;
+b1 || b1;
+!b1;
+
+// misc
+typeof a;
+
+// instanceof
+
+if (true) {
+	a+1;
+}
